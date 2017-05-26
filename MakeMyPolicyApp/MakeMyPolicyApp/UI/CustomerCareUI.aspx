33193891af021763f4db8a850335ceef1f1394c0
RozyAfroza <dilafroza2012@gmail.com>
2017-05-26 10:39:50 +0600

    Done Successfully

---
+++ MakeMyPolicyApp/MakeMyPolicyApp/UI/CustomerCareUI.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/UI/Home.Master" AutoEventWireup="true" CodeBehind="CustomerCareUI.aspx.cs" Inherits="MakeMyPolicyApp.UI.CustomerCareUI" %>
+
+
+<asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
+	<% Session["PageName"] = "CustomerCareUI"; %>
+</asp:Content>
+<asp:Content ID="Content6" ContentPlaceHolderID="content" runat="server">
+	<div class="container big-card">
+		<div class="row">
+			<div class="col-5">
+				<img align="right" src="../Contents/helpline_icon.png" alt="Help Line" height="150px" width="170px"/>
+			</div>
+			<div class="col-7">
+				<div class="row">
+					<h5 class="offset-1" style="color: burlywood">Contact with us:</h5>
+					<br />
+				</div>
+				<div class="row">
+					<span class="offset-1">042527</span><br />
+				</div>
+				<div class="row">
+					<span class="offset-1">0882567</span><br />
+				</div>
+				<div class="row">
+					<span class="offset-1">01706325976</span><br />
+				</div>
+				<div class="row">
+					<span class="offset-1">01843199582</span><br />
+				</div>
+			</div>
+		</div>
+		<hr />
+		<div class="form-group row">
+			<div class="offset-1 col-4">
+				<h5 class="control-label" for="commentTextArea" runat="server" style="color: lightseagreen">Leave a comment:</h5>
+			</div>
+		</div>
+
+		<div class="form-group row">
+			<div class="offset-1 col-2">
+				<label class="control-label " for="commentTitleInputBox" runat="server">Title<abbr title="Required" class="required"></abbr></label>
+			</div>
+			<div class="col-8">
+				<input class="form-control mandatory jqValidate" name="NID" type="text" id="commentTitleInputBox" runat="server" placeholder="Type Comment Title" />
+				<label class="validation-summary-errors field-validation-error" style="display: none;"></label>
+			</div>
+		</div>
+
+		<div class="form-group row">
+			<div class="offset-1 col-sm-2">
+				<label class="control-label " for="commentTextArea" runat="server">Comment<abbr title="Required" class="required"></abbr></label>
+			</div>
+			<div class="col-8">
+				<textarea class="form-control mandatory jqValidate" name="Address" id="commentTextArea" runat="server" placeholder="Type Comment" style="height: 100px;"></textarea>
+				<label class="validation-summary-errors field-validation-error" style="display: none;"></label>
+			</div>
+		</div>
+		<div class="form-group row">
+			<div class="offset-5 col-sm-7">
+				<asp:Button CssClass="btn btn-primary feedbackAlert" ID="submitButton" runat="server" Text="Submit" OnClientClick="javascript:return inputFieldValidation();" OnClick="submitButton_Click" />
+			</div>
+		</div>
+	</div>
+</asp:Content>
---
+++ MakeMyPolicyApp/MakeMyPolicyApp/UI/Home.Master.cs
@@ -0,0 +1,155 @@
+﻿using MakeMyPolicyApp.BLL;
+using MakeMyPolicyApp.ModelClass;
+using System;
+using System.Linq;
+using System.Net;
+
+namespace MakeMyPolicyApp.UI
+{
+	public partial class Home : System.Web.UI.MasterPage
+	{
+		private SignupManager _signupManager = new SignupManager();
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+
+		protected void Login_Button_Click(object sender, EventArgs e)
+		{
+			string userNameOrEmail = userNameLoginInputBox.Value;
+			string password = passwordLoginInputBox.Value;
+
+			User user = _signupManager.LoginUser(userNameOrEmail, password);
+
+			if (user != null)
+			{
+				Session["UserId"] = user.Id;
+				Session["UserName"] = user.FullName;
+				Session["UserType"] = "Me";
+				Response.Cookies["UserId"].Value = user.Id.ToString();
+				Response.Cookies["UserName"].Value = user.FullName;
+				Response.Cookies["UserType"].Value = "Me";
+				Response.Cookies["UserId"].Expires = DateTime.Now.AddYears(1);
+				Response.Cookies["UserName"].Expires = DateTime.Now.AddYears(1);
+				Response.Cookies["UserType"].Expires = DateTime.Now.AddYears(1);
+				var smtp = new System.Net.Mail.SmtpClient();
+				{
+					smtp.Host = "smtp.gmail.com";
+					smtp.Port = 587;
+					smtp.EnableSsl = true;
+					smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+					smtp.Credentials = new NetworkCredential("makemypolicy472", "123monjur");
+					smtp.Timeout = 20000;
+				}
+				// Passing values to smtp object
+				try
+				{
+					smtp.Send("makemypolicy472@gmail.com", user.Email, "Subject", "Mail body");
+				}
+				catch (Exception exception)
+				{
+					//Console.WriteLine(exception);
+					//throw;
+				}
+
+				Response.Redirect("HomeUI.aspx");
+
+
+				//Response.Write("mail Send");
+				//document.getElementById('login-validation-message').innerHTML = 'Wrong User Name or Password!';
+			}
+			else
+			{
+				Session["ErrorMessage"] = "Wrong User Name or Password!";
+				//string myScriptValue = "function callMe() {alert('You pressed Me!'); }";
+				//ScriptManager.RegisterStartupScript(this, Page.GetType(), "key", "javascript:MyFunction()", true);
+				//Response.Write("<script>alert('userNameLoginInputBox');</script>");
+			}
+		}
+
+		protected void LogoutButton_Click(object sender, EventArgs e)
+		{
+			Session["UserId"] = null;
+			Session["UserName"] = null;
+			Session["UserType"] = null;
+			Session.Clear();
+			Response.Cookies["UserId"].Expires = DateTime.Now.AddDays(-1);
+			Response.Cookies["UserName"].Expires = DateTime.Now.AddDays(-1);
+			Response.Cookies["UserType"].Expires = DateTime.Now.AddDays(-1);
+
+			Response.Redirect("HomeUI.aspx");
+		}
+
+		protected void AdminLoginButton_Click(object sender, EventArgs e)
+		{
+			string userNameOrEmail = userNameLoginInputBox.Value;
+			string password = passwordLoginInputBox.Value;
+
+			//User user = _signupManager.LoginUser(userNameOrEmail, password);
+			string[] adminAccess = new[] { "monjurul", "789", "biplob_zaman", "123", "mahmud_pulap", "456", "khanom.sumaiya", "6969" };
+
+			for (int i = 0; i < adminAccess.Length; i += 2)
+			{
+				if (userNameOrEmail == adminAccess[i] && password == adminAccess[i + 1])
+				{
+					char[] delimiterChars = { '_', '.' };
+					string[] nameArray = adminAccess[i].Split(delimiterChars);
+					string userName;
+
+					if (nameArray.Length > 1)
+					{
+						userName = nameArray[0].First().ToString().ToUpper() + nameArray[0].Substring(1) + " " + nameArray[1];
+					}
+					else
+					{
+						userName = nameArray[0].First().ToString().ToUpper() + nameArray[0].Substring(1);
+					}
+
+					Session["UserId"] = 0;
+					Session["UserName"] = userName;
+					Session["UserType"] = "Admin";
+					Response.Cookies["UserId"].Value = 0.ToString();
+					Response.Cookies["UserName"].Value = userName;
+					Response.Cookies["UserType"].Value = "Admin";
+					Response.Cookies["UserId"].Expires = DateTime.Now.AddYears(1);
+					Response.Cookies["UserName"].Expires = DateTime.Now.AddYears(1);
+					Response.Cookies["UserType"].Expires = DateTime.Now.AddYears(1);
+					/*var smtp = new System.Net.Mail.SmtpClient();
+					{
+						smtp.Host = "smtp.gmail.com";
+						smtp.Port = 587;
+						smtp.EnableSsl = true;
+						smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+						smtp.Credentials = new NetworkCredential("makemypolicy472", "123monjur");
+						smtp.Timeout = 20000;
+					}
+					// Passing values to smtp object
+					try
+					{
+						smtp.Send("makemypolicy472@gmail.com", user.Email, "Subject", "Mail body");
+					}
+					catch (Exception exception)
+					{
+						//Console.WriteLine(exception);
+						//throw;
+					}*/
+
+					Response.Redirect("AgentMessageListUI.aspx");
+					break;
+
+					//Response.Write("mail Send");
+					//document.getElementById('login-validation-message').innerHTML = 'Wrong User Name or Password!';
+				}
+				else
+				{
+					Session["ErrorMessage"] = "Wrong User Name or Password!";
+					//string myScriptValue = "function callMe() {alert('You pressed Me!'); }";
+					//ScriptManager.RegisterStartupScript(this, Page.GetType(), "key", "javascript:MyFunction()", true);
+					//Response.Write("<script>alert('userNameLoginInputBox');</script>");
+				}
+			}
+		}
+	}
+}
